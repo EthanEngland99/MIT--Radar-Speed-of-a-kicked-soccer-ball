ece8cf30eff9ed1d15e854667d3253ad764ca951
EthanEngland99 <ethanrengland@gmail.com>
2024-09-25 08:54:43 +0200

    Update Ethan_CFAR.m
    Changes to detection algorithm, further coming soon

--- Ethan_CFAR.m
+++ Ethan_CFAR.m
@@ -1,45 +1,31 @@
-function[DataAfterPowerLawDetector, Threshold, detections] = Ethan_CFAR(S_FewColumns, f, t)
-
-% len = 100000;   % Signal Length
-PFA = 10^-3;    % Probability of False Alarm
-RefWindow = 64; % Reference Window or CFAR Window
-Training = RefWindow/2;  %training cells
-guard = 4;
+function[DataAfterSquareLawDetector, Threshold, detections] = Ethan_CFAR(S_FewColumns,PFA, RefWindow, Training, guard)
 
 
-DataAfterPowerLawDetector = abs(S_FewColumns).^2;
+DataAfterSquareLawDetector = abs(S_FewColumns).^2; % Data after Square Law Detector (|S|^2)
 
-[fin, numCols] = size(S_FewColumns);
+[fin, numCols] = size(S_FewColumns); %calculating size of matrices needed
 
 alphaCA = RefWindow*(PFA^(-1/RefWindow) - 1); % alpha (CFAR) constant calculation 
 
 Threshold = zeros(fin,numCols);  %initialising threshold matrix
 g = zeros(fin,numCols); % initialising g_hat matrix
-detections =  zeros(fin, numCols);
-
-% numFalseAlarm = 0; % starting number for false alarm detections
+detections =  zeros(fin, numCols); % initialising detection matrix
 
 for col = 1:numCols
-    for CUT = Training + guard+1 : fin-Training-guard
+    for CUT = Training + guard+1 : fin-Training-guard %calculating the Cell Under Test
         laggingWindow = CUT-Training-guard : CUT-guard-1; %Lagging window Index
         leadingWindow = CUT+guard+1 : CUT+Training+guard; % Leading window index
-
-        g(CUT, col) = (sum(DataAfterPowerLawDetector(laggingWindow,col)) + sum(DataAfterPowerLawDetector(leadingWindow,col)))/RefWindow; %sum of leading window + sum of lagging window
+        
+        %sum of leading window + sum of lagging window
+        g(CUT, col) = (sum(DataAfterSquareLawDetector(laggingWindow,col)) + sum(DataAfterSquareLawDetector(leadingWindow,col)))/RefWindow; 
      
         Threshold(CUT,col) = alphaCA*g(CUT,col); % populating the threshold matrx
 
-        if DataAfterPowerLawDetector(CUT,col) > Threshold(CUT,col) % If a signal is detected above the threshold, count one extra into CFARout
-            %numFalseAlarm = numFalseAlarm + 1;
-            detections(CUT,col) = 1;
+        if DataAfterSquareLawDetector(CUT,col) > Threshold(CUT,col) % If a signal is detected above the threshold, count one extra into CFARout
+            detections(CUT,col) = 1; % adding 1 to detections matrix for each data point > theshold
         end
 
     end
 end
-
-% PFA_Simulation = numFalseAlarm/(fin*tin);
-% PFA_error = abs((PFA - PFA_Simulation)/PFA*100);
-% 
-% disp("Simulation PFA: " + PFA_Simulation)
-% disp("PFA Error: " + PFA_error)
 end
 
